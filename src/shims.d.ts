59edad80ded5e8e8f93f78b6d2c4b83049d44d54
ShareVB <sharevb@gmail.com>
2026-02-01 12:52:28 +0100

    feat(new tool): RSVP Reader
    Fix #306

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -78,3 +78,56 @@ namespace Intl {
     format(duration: { seconds?: number; milliseconds?: number }): string;
   }
 }
+
+declare module '@vueuse/integrations/useIDBKeyval' {
+  interface Serializer<T> {
+    read: (raw: unknown) => T
+    write: (value: T) => unknown
+  }
+  export interface UseIDBOptions<T> extends ConfigurableFlush {
+    /**
+     * Watch for deep changes
+     *
+     * @default true
+     */
+    deep?: boolean
+    /**
+     * On error callback
+     *
+     * Default log error to `console.error`
+     */
+    onError?: (error: unknown) => void
+    /**
+     * Use shallow ref as reference
+     *
+     * @default false
+     */
+    shallow?: boolean
+    /**
+     * Write the default value to the storage when it does not exist
+     *
+     * @default true
+     */
+    writeDefaults?: boolean
+    /**
+     * Custom data serialization
+     */
+    serializer?: Serializer<T>
+  }
+  export interface UseIDBKeyvalReturn<T> {
+    data: RemovableRef<T>
+    isFinished: ShallowRef<boolean>
+    set: (value: T) => Promise<void>
+  }
+  /**
+   *
+   * @param key
+   * @param initialValue
+   * @param options
+   */
+  export declare function useIDBKeyval<T>(
+    key: IDBValidKey,
+    initialValue: MaybeRefOrGetter<T>,
+    options?: UseIDBOptions<T>,
+  ): UseIDBKeyvalReturn<T>
+}

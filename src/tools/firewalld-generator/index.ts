4816cbc58ef2305e78446c101561594cd220c64f
ShareVB <sharevb@gmail.com>
2026-03-07 20:28:48 +0100

    fix(Firewalld Generator): rename title and description

--- src/tools/firewalld-generator/index.ts
+++ src/tools/firewalld-generator/index.ts
@@ -3,10 +3,10 @@ import { defineTool } from '../tool';
 import { translate as t } from '@/plugins/i18n.plugin';
 
 export const tool = defineTool({
-  name: t('tools.firewalld-generator.title'),
+  name: t('tools.firewalld-generator.title2'),
   path: '/firewalld-generator',
-  description: t('tools.firewalld-generator.description'),
-  keywords: ['firewalld', 'fw'],
+  description: t('tools.firewalld-generator.description2'),
+  keywords: ['firewalld', 'firewall-cmd'],
   component: () => import('./firewalld-generator.vue'),
   icon: Firetruck,
   createdAt: new Date('2025-04-21'),
